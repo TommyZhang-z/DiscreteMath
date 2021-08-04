613dadaeb4ad24a50cc9f2912c0601810bb0851d
Wentao Zhang <zwt03712@gmail.com>
2021-08-04 15:10:40 +1000

    update python code

--- logic_calculator.py
+++ logic_calculator.py
@@ -1,33 +1,11 @@
-class MyInt(int):
-    def MI(self, q):
-        """
-        material implication
-        单向导致 p -> q
-        :param q:
-        :return:逻辑判断结果
-        """
-        return MyInt(not self or q)
-
-
-def T(T_list):
+def mi(p, q):
     """
-    结果全为True
-    :param T_list:
-    :return: bool
+    material implication
+    单向导致 p -> q
+    :param q:
+    :return:逻辑判断结果
     """
-    return all(T_list)
-
-
-def zeros(num):
-    return [MyInt(0) for _ in range(num)]
-
-
-def ones(num):
-    return [MyInt(1) for _ in range(num)]
-
-
-def array(list_):
-    return [MyInt(num) for num in list_]
+    return not p or q
 
 
 def test(*args, func) -> list:
