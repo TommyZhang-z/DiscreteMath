189f1b105312730e820617931af3f78581775ccf
Wentao Zhang <zwt03712@gmail.com>
2021-08-04 16:47:46 +1000

    update code and const

--- logic_calculator.py
+++ logic_calculator.py
@@ -1,4 +1,30 @@
-def imp(p, q):
+import numpy as np
+
+# defining constants
+P = zip(
+    np.array([1, 0]),
+)
+
+PQ = zip(
+    np.array([1, 1, 0, 0]),
+    np.array([1, 0, 1, 0])
+)
+
+PQR = zip(
+    np.array([1, 1, 1, 1, 0, 0, 0, 0]),
+    np.array([1, 1, 0, 0, 1, 1, 0, 0]),
+    np.array([1, 0, 1, 0, 1, 0, 1, 0])
+)
+
+PQRS = zip(
+    np.array([1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]),
+    np.array([1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0]),
+    np.array([1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0]),
+    np.array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0])
+)
+
+
+def imp(p, q) -> bool:
     """
     material implication
     单向导致 p -> q
@@ -9,14 +35,17 @@ def imp(p, q):
     return not p or q
 
 
-def test(*args, func) -> list:
+def test(zipVar, func) -> np.ndarray:
     """
     合成函数
-    :param args: 变量列表
+    :param zipVar: 变量列表的zip
     :param func: 执行函数
     :return: 逻辑判断结果
     """
     truth_value = []
-    for var in zip(*args):
+    for var in zipVar:
         truth_value.append(int(func(*var)))
-    return truth_value
+    result = np.array(truth_value)
+    truth_value.clear()
+    return result
+
